6c66ea99ce0888a0f70ef815acf671c7d3e6d7fd
BOTOOM <eddiazr@correo.udistrital.edu.co>
2019-02-05 21:27:18 -0500

    correccion date

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { LOCALE_ID } from '@angular/core';
 import { AppComponent } from './app.component';
+import localeEs from '@angular/common/locales/es-MX';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localeEs);
 
 @NgModule({
   declarations: [
@@ -11,7 +14,7 @@ import { AppComponent } from './app.component';
     BrowserModule
   ],
   providers: [
-    { provide: LOCALE_ID, useValue: "es" }
+    { provide: LOCALE_ID, useValue: 'es-MX' }
   ],
   bootstrap: [AppComponent]
 })
